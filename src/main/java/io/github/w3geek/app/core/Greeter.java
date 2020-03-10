d5b042598dcbcda7a24d376b0f7b595b44aa3f48
Ryan Clark <rclark@routeone.com>
2020-03-10 16:36:59 -0400

    Task #1 Say hello to user with a personal greeting when user's name is provided.

--- src/main/java/io/github/w3geek/app/core/Greeter.java
+++ src/main/java/io/github/w3geek/app/core/Greeter.java
@@ -1,19 +1,49 @@
 package io.github.w3geek.app.core;
 
+import io.github.w3geek.app.config.Constants;
+
 import java.io.PrintStream;
 
 public class Greeter {
+	private boolean lineSeparatorInserted;
 	private PrintStream stdout;
+	private String user = Constants.DEFAULT_USER;
 
 	public Greeter() {
-		this.stdout = System.out;
+		this(System.out, true);
 	}
 
 	protected Greeter(PrintStream stdout) {
+		this(stdout, false);
+	}
+
+	protected Greeter(PrintStream stdout, boolean lineSeparatorInserted) {
 		this.stdout = stdout;
+		setLineSeparatorInserted(lineSeparatorInserted);
+	}
+
+	public boolean isLineSeparatorInserted() {
+		return lineSeparatorInserted;
+	}
+
+	public void setLineSeparatorInserted(boolean lineSeparatorInserted) {
+		this.lineSeparatorInserted = lineSeparatorInserted;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
 	}
 
 	public void greet() {
-		stdout.println("Hello, world!");
+		String format = "Hello, %s!";
+		if (lineSeparatorInserted) {
+			format = (format + "%n");
+		}
+
+		stdout.printf(format, user);
 	}
 }
--- src/test/java/io/github/w3geek/app/core/GreeterTest.java
+++ src/test/java/io/github/w3geek/app/core/GreeterTest.java
@@ -8,27 +8,29 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.endsWith;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.isEmptyString;
 
 public class GreeterTest {
 	private ByteArrayOutputStream greeterOutputStream;
+	private PrintStream greeterPrintStream;
 	private Greeter greeter;
 
 	@Before
 	public void setUp() {
 		greeterOutputStream = new ByteArrayOutputStream();
-		greeter = new Greeter(new PrintStream(greeterOutputStream));
+		greeterPrintStream = new PrintStream(greeterOutputStream);
+		greeter = new Greeter(greeterPrintStream);
 	}
 
 	@After
-	public void tearDown() throws Exception {
-		try {
-			greeterOutputStream.close();
-		} finally {
-			greeterOutputStream = null;
-			greeter = null;
-		}
+	public void tearDown() {
+		greeterPrintStream.close();
+		greeterPrintStream = null;
+		greeterOutputStream = null;
+		greeter = null;
 	}
 
 	@Test
@@ -42,6 +44,16 @@ public class GreeterTest {
 		greeter.greet();
 
 		String greeterOutputStreamData = greeterOutputStream.toString();
+		assertThat(greeterOutputStreamData, is("Hello, world!"));
+	}
+
+	@Test
+	public void testWhenGreetHasBeenInvoked_withLineSeparatorInsertedIsTrue_greetingShouldNotBeEmpty() {
+		greeter.setLineSeparatorInserted(true);
+		greeter.greet();
+
+		String greeterOutputStreamData = greeterOutputStream.toString();
 		assertThat(greeterOutputStreamData, containsString("Hello, world!"));
+		assertThat(greeterOutputStreamData, endsWith(System.lineSeparator()));
 	}
 }
